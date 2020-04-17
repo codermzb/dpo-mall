7f78c1a22beb9ebe7823c5866204a8c8dc47bb1f
codermzb <1072192327@qq.com>
2020-04-17 10:44:50 +0800

    '更改url地址'

--- src/network/request.js
+++ src/network/request.js
@@ -1,8 +1,9 @@
 import axios from 'axios'
 export function request(config) {
   const instance1 = axios.create({
-    baseURL: 'http://123.207.32.32:8000/api/hy',
-    // baseURL: 'http://106.54.54.237:8000/api/hy',
+    // baseURL: 'http://152.136.185.210:8000/api/h8',
+    baseURL: 'http://123.207.32.32:8000/api/h8',
+    // baseURL: 'http://106.54.54.237:8000/api/h8',
     timedOut:5000
   })
   instance1.interceptors.request.use(config => {
--- src/views/profile/Profile.vue
+++ src/views/profile/Profile.vue
@@ -1,15 +1,20 @@
 <template>
     <div id="profile">
-     <h2>我的</h2>
+        <div class="father">
+          <div class="son">
+            喔喔
+          </div>
+        </div>
     </div>
 </template>
 
 <script>
   export default {
-    name: "Profile"
+    name: "Profile",
+    components: {
+    }
   }
 </script>
 
 <style scoped lang="less">
-
 </style>
